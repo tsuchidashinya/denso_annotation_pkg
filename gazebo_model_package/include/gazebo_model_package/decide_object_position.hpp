a50242ec5d0005ae6e40cd80129292f9e0975f62
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2022-09-11 00:00:24 +0900

    fix: rename file

---
+++ gazebo_model_package/include/gazebo_model_package/decide_object_position.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file decide_object_position.hpp
+ * @author tsuchidashinya (tsuchida.shinya413@mail.kyutech.jp)
+ * @brief
+ * @version 0.1
+ * @date 2022-09-10
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#pragma once
